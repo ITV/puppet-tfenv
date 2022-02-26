58e17760f83ae825e3e2abdfc581ce9eea9a0542
Anne Thorniley <anne.thorniley@itv.com>
2022-02-26 10:10:19 +0000

    update tfenv github repo location

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@
 #
 class tfenv::params {
     $install_dir    = '/opt/tfenv'
-    $tfenv_git_repo = 'https://github.com/kamatama41/tfenv'
+    $tfenv_git_repo = 'https://github.com/tfutils/tfenv'
     $tfenv_revision = 'v0.5.2'
     $tfenv_user     = 'jenkins'
     $tfenv_group    = 'jenkins'
